07016c875e8bafa3aec081594177b389b38f7971
movie0world <63150363+movie0world@users.noreply.github.com>
2020-12-02 15:00:48 +0500

    this is gud

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 
-app.set("views", path.join(__dirname, "public"));
+app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs')
 
 
